dcef3a160a9fb468c879d2152b302b392a6c9cb0
mlordi <mlordi80@hotmail.com>
2024-12-31 12:31:47 -0500

    Update Tool.vue

--- resources/js/components/Tool.vue
+++ resources/js/components/Tool.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h4 class="text-90 font-normal text-2xl mb-3">
-            Comments
+            Chat
         </h4>
 
         <div class="bg-white dark:bg-gray-800 rounded-lg shadow pt-2">
@@ -12,11 +12,12 @@
                     </h4>
 
                     <textarea class="w-full form-control form-input form-input-bordered py-3 h-auto mt-2"
-                    id="commenter"
-                    dusk="commenter"
-                    rows="5"
-                    v-model="comment"
-                    @keydown.enter.meta="createComment">
+                              id="commenter"
+                              dusk="commenter"
+                              rows="5"
+                              v-model="comment"
+                              @keydown.enter.meta="createComment"
+                              :disabled="isLoading">
                     </textarea>
                 </div>
             </div>
@@ -27,16 +28,16 @@
                 </div>
 
                 <button class="flex-shrink-0 shadow rounded focus:outline-none focus:ring bg-primary-500 hover:bg-primary-400 active:bg-primary-600 text-white dark:text-gray-800 inline-flex items-center font-bold px-4 h-9 text-sm flex-shrink-0 mb-3 mt-4"
-                    type="submit"
-                    @click="createComment"
-                    :disabled="isSubmitting">
-                    Save Comment
+                        type="submit"
+                        @click="createComment"
+                        :disabled="isLoading"
+                        v-html="isLoading ? 'Saving...' : 'Save Comment'">
                 </button>
             </div>
 
             <div class="flex border-b border-40 remove-bottom-border px-8" v-if="hasComments">
                 <div class="w-full py-6">
-                    <h3 class="text-90 font-bold text-lg mb-4">Comments</h3>
+                    <h3 class="text-90 font-bold text-lg mb-4">Chat</h3>
 
                     <comment :comment="comment" v-for="(comment, key) in data.resources" :key="key"></comment>
                 </div>
@@ -45,16 +46,16 @@
             <div class="bg-20 rounded-b" v-if="hasPagination">
                 <nav class="flex justify-between items-center">
                     <button class="text-xs font-bold py-3 px-4 focus:outline-none rounded-bl-lg focus:ring focus:ring-inset"
-                        :class="paginationClass(hasNextLink)"
-                        :disabled="! hasNextLink"
-                        @click="getComments(data.next_page_url)">
+                            :class="paginationClass(hasNextLink)"
+                            :disabled="!hasNextLink"
+                            @click="getComments(data.next_page_url)">
                         Older
                     </button>
 
                     <button class="text-xs font-bold py-3 px-4 focus:outline-none rounded-bl-lg focus:ring focus:ring-inset"
-                        :class="paginationClass(hasPrevLink)"
-                        :disabled="! hasPrevLink"
-                        @click="getComments(data.prev_page_url)">
+                            :class="paginationClass(hasPrevLink)"
+                            :disabled="!hasPrevLink"
+                            @click="getComments(data.prev_page_url)">
                         Newer
                     </button>
                 </nav>
@@ -64,97 +65,98 @@
 </template>
 
 <script>
-    import Comment from './Comment';
+import Comment from './Comment';
 
-    export default {
-        props: ['resourceName', 'resourceId', 'field'],
+export default {
+    props: ['resourceName', 'resourceId', 'field'],
 
-        components: { Comment },
+    components: { Comment },
 
-        data() {
-            return {
-                baseCommentUri: '/nova-api/comments',
-                comment: '',
-                data: {
-                    next_page_url: '',
-                    prev_page_url: '',
-                    resources: {},
-                    isSubmitting: false
-                }
-            }
-        },
-
-        mounted() {
-            this.getComments(this.commentsUri);
-        },
-
-        computed: {
-            commentsUri() {
-                return `${this.baseCommentUri}?page=1`;
-            },
-
-            hasComments() {
-                return Boolean(this.data.resources.length);
-            },
-
-            hasNextLink() {
-                return Boolean(this.data.next_page_url);
-            },
-
-            hasPrevLink() {
-                return Boolean(this.data.prev_page_url);
-            },
-
-            hasPagination() {
-                return this.hasNextLink || this.hasPrevLink;
-            },
-
-            queryParams() {
-                return `&orderBy=created_at&orderByDirection=desc&viaResource=${this.resourceName}&viaResourceId=${this.resourceId}&viaRelationship=comments&relationshipType=hasMany`;
-            },
-        },
-
-        methods: {
-            createComment() {
-                this.isSubmitting = true;
-                
-                if (! this.comment) {
-                    return false;
-                }
-
-                let payload = {
-                    comment: this.comment,
-                    viaResource: this.resourceName,
-                    viaResourceId: this.resourceId,
-                    viaRelationship: 'comments',
-                };
-
-                Nova.request().post(this.baseCommentUri, payload)
-                    .then(() => {
-                        this.getComments(this.commentsUri);
-
-                        this.resetComment();
-
-                        Nova.success(`A new comment has been created.`);
-                        location.reload();
-                    })
-                    .catch(response => Nova.error(response));
-            },
-
-            getComments(uri) {
-                Nova.request().get(`${uri}${this.queryParams}`)
-                    .then(({ data }) => this.data = data);
-            },
-
-            paginationClass(isActive) {
-                return isActive
-                    ? 'text-primary-500 hover:text-primary-400 active:text-primary-600'
-                    : 'text-gray-300 dark:text-gray-600';
-            },
-
-            resetComment() {
-                this.comment = '';
+    data() {
+        return {
+            baseCommentUri: '/nova-api/comments',
+            comment: '',
+            isLoading: false,
+            data: {
+                next_page_url: '',
+                prev_page_url: '',
+                resources: {},
             }
         }
+    },
+
+    mounted() {
+        this.getComments(this.commentsUri);
+    },
+
+    computed: {
+        commentsUri() {
+            return `${this.baseCommentUri}?page=1`;
+        },
+
+        hasComments() {
+            return Boolean(this.data.resources.length);
+        },
+
+        hasNextLink() {
+            return Boolean(this.data.next_page_url);
+        },
+
+        hasPrevLink() {
+            return Boolean(this.data.prev_page_url);
+        },
+
+        hasPagination() {
+            return this.hasNextLink || this.hasPrevLink;
+        },
+
+        queryParams() {
+            return `&orderBy=created_at&orderByDirection=desc&viaResource=${this.resourceName}&viaResourceId=${this.resourceId}&viaRelationship=comments&relationshipType=hasMany`;
+        },
+    },
+
+    methods: {
+        createComment() {
+            if (this.isLoading || !this.comment) {
+                return false;
+            }
+
+            this.isLoading = true;
+
+            let payload = {
+                comment: this.comment,
+                viaResource: this.resourceName,
+                viaResourceId: this.resourceId,
+                viaRelationship: 'comments',
+            };
+
+            Nova.request().post(this.baseCommentUri, payload)
+                .then(() => {
+                    this.getComments(this.commentsUri);
+                    this.resetComment();
+                    Nova.success(`A new comment has been created.`);
+                    location.reload();
+                })
+                .catch(response => Nova.error(response))
+                .finally(() => {
+                    this.isLoading = false;
+                });
+        },
+
+        getComments(uri) {
+            Nova.request().get(`${uri}${this.queryParams}`)
+                .then(({ data }) => this.data = data);
+        },
+
+        paginationClass(isActive) {
+            return isActive
+                ? 'text-primary-500 hover:text-primary-400 active:text-primary-600'
+                : 'text-gray-300 dark:text-gray-600';
+        },
+
+        resetComment() {
+            this.comment = '';
+        }
     }
+}
 </script>
